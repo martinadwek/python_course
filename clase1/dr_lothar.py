e6256f29c3d4a4e900e551ec9308a44797319578
Matias Dwek <matiasdwek@gmail.com>
2020-10-11 22:49:43 +0200

    Add rec Dr Lothar

--- clase1/dr_lothar.py
+++ clase1/dr_lothar.py
@@ -10,6 +10,18 @@ def dr_lothar(number: int) -> int:
             number = number * 3 + 1
     return steps
 
-# x = int(input("Number for Dr Lothar: "))
+
+def dr_lothar_rec(number: int, steps: int) -> int:
+    if number <= 0:
+        raise ValueError("Input must be > 0")
+    if number == 1:
+        return steps
+    if not number % 2:
+        return dr_lothar_rec(int(number / 2), steps + 1)
+    else:
+        return dr_lothar_rec(number * 3 + 1, steps + 1)
+
+
+# x = int(input(""))
 # solution = dr_lothar(x)
 # print(solution)
--- clase1/test_dr_lothar.py
+++ clase1/test_dr_lothar.py
@@ -1,9 +1,9 @@
 import unittest
 
-from clase1.dr_lothar import dr_lothar
+from clase1.dr_lothar import dr_lothar, dr_lothar_rec
 
 
-class MyTestCase(unittest.TestCase):
+class DrLotharTestCase(unittest.TestCase):
     def test_even_number(self):
         self.assertEqual(8, dr_lothar(6))
 
@@ -13,6 +13,18 @@ class MyTestCase(unittest.TestCase):
     def test_leq_zero_number(self):
         with self.assertRaises(ValueError):
             dr_lothar(-1)
+
+
+class DrLotharRecTestCase(unittest.TestCase):
+    def test_even_number(self):
+        self.assertEqual(8, dr_lothar_rec(6, 0))
+
+    def test_odd_number(self):
+        self.assertEqual(7, dr_lothar_rec(3, 0))
+
+    def test_leq_zero_number(self):
+        with self.assertRaises(ValueError):
+            dr_lothar_rec(-1, 0)
 
 
 if __name__ == '__main__':
